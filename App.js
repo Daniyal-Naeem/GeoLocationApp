b6456bdae77d34ec4bc20940687406c16205e6f8
Daniyal Naeem <daniyalnaeem5@gmail.com>
2021-08-23 13:30:13 +0500

    updated code for GeoLocation

--- App.js
+++ App.js
@@ -1,16 +1,50 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {
   Text,
   View,
+  PermissionsAndroid
 } from 'react-native';
 import Geolocation from 'react-native-geolocation-service';
 
 const App = () => {
 
-  const [state, setstate] = useState(0)
-  Geolocation.getCurrentPosition(data => {
-    setstate(data.coords.latitude)
-  })
+  const requestLocationPermission = async () => {
+    try {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+        {
+          title: "App Location Permission",
+          message:
+            "This App needs access to your Location ",
+          buttonNeutral: "Ask Me Later",
+          buttonNegative: "Cancel",
+          buttonPositive: "OK"
+        }
+      );
+      return granted === PermissionsAndroid.RESULTS.GRANTED
+     
+    } catch (err) {
+      console.warn(err);
+      return false
+    }
+  };
+
+  const [state, setState] = useState(0)
+
+  const getLocation = async ()=>{
+    const status = await requestLocationPermission()
+    
+    if (status){
+      Geolocation.getCurrentPosition(data => {
+        setState(data.coords.latitude.toString() + "," + data.coords.longitude.toString())
+      })
+      
+    }
+  }
+
+  useEffect(() =>{
+    getLocation()
+  }, [])
   return (
   <View>
     <Text style={{fontSize:30}}> 
