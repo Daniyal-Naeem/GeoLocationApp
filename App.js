9047cfdea5213088ab5a3107a37d31b334988ce8
Daniyal Naeem <daniyalnaeem5@gmail.com>
2021-08-24 11:49:41 +0500

    updated App.js

--- App.js
+++ App.js
@@ -4,53 +4,54 @@ import {
   View,
   PermissionsAndroid
 } from 'react-native';
-import Geolocation from 'react-native-geolocation-service';
+//import Geolocation from 'react-native-geolocation-service';
+import GeolocationExample from './GeoLocation'
 
 const App = () => {
 
-  const requestLocationPermission = async () => {
-    try {
-      const granted = await PermissionsAndroid.request(
-        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
-        {
-          title: "App Location Permission",
-          message:
-            "This App needs access to your Location ",
-          buttonNeutral: "Ask Me Later",
-          buttonNegative: "Cancel",
-          buttonPositive: "OK"
-        }
-      );
-      return granted === PermissionsAndroid.RESULTS.GRANTED
+  // const requestLocationPermission = async () => {
+  //   try {
+  //     const granted = await PermissionsAndroid.request(
+  //       PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+  //       {
+  //         title: "App Location Permission",
+  //         message:
+  //           "This App needs access to your Location ",
+  //         buttonNeutral: "Ask Me Later",
+  //         buttonNegative: "Cancel",
+  //         buttonPositive: "OK"
+  //       }
+  //     );
+  //     return granted === PermissionsAndroid.RESULTS.GRANTED
      
-    } catch (err) {
-      console.warn(err);
-      return false
-    }
-  };
+  //   } catch (err) {
+  //     console.warn(err);
+  //     return false
+  //   }
+  // };
 
-  const [state, setState] = useState(0)
+  // const [state, setState] = useState(0)
 
-  const getLocation = async ()=>{
-    const status = await requestLocationPermission()
-    
-    if (status){
-      Geolocation.getCurrentPosition(data => {
-        setState(data.coords.latitude.toString() + "," + data.coords.longitude.toString())
-      })
+  // const getLocation = async ()=>{
+  //   const status = await requestLocationPermission()
+  //   if (status){
+  //     Geolocation.getCurrentPosition(data => {
+  //       setState(data.coords.latitude.toString() + "," + data.coords.longitude.toString())
+  //     })
       
-    }
-  }
+  //   }
+  // }
+  // useEffect(() =>{
+  //   getLocation()
+  // }, [])
 
-  useEffect(() =>{
-    getLocation()
-  }, [])
   return (
-  <View>
-    <Text style={{fontSize:30}}> 
-     latitude: {state}
-    </Text>
-  </View>
+    <GeolocationExample/>
+  // <View style={{ flexGrow: 1, alignItems: 'center', justifyContent: 'center' }} >
+  //   <Text style={{fontSize:30}}> 
+  //    latlong: {state}
+  //   </Text>
+  // </View>
   )
 };
 
